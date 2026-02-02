7a71b40e7d875aad1820cce3c692a8d5752208ec
2500030460 <2500030460@kluniversity.in>
2026-02-02 09:29:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "80px", fontFamily: "Arial" }}>
+      <h1>Hello World</h1>
+      <p>Welcome to React + Vite 🚀</p>
+    </div>
+  );
+}
+
+export default App;
